4d83fd93b765f403d909810e44543f5a61f9eec1
Hey Guy <whatisthatyouaredoing@gmail.com>
2014-05-26 22:39:23 -0600

    Updated

--- build/buildBook.sh
+++
@@ -1,9 +0,0 @@
-cd /home/user/wheel/book
-git clone https://github.com/cassidyk/whatcomesbeforealpha.git
-gitbook build ./whatcomesbeforealpha/ --output=./output
-cp -R ./output/* ./cassidyk.github.io/
-rm -rf whatcomesbeforealpha/ output/
-cd ./cassidyk.github.io
-git add .
-git commit -m 'auto build'
-git push
--- build/run.sh
+++
@@ -1,8 +0,0 @@
-curl -u \
-whatisthatyouaredoing:thisismypassword,therearemanylikeitbutthisoneismine \
---silent "https://mail.google.com/mail/feed/atom" > \
-/home/user/wheel/book/mail.txt
-ruby /home/user/wheel/book/ruby.rb
-rm -f /home/user/wheel/book/mail.txt
-#timedatectl status | grep Local  | tr -d ' ' > \
-#/home/user/wheel/book/time
